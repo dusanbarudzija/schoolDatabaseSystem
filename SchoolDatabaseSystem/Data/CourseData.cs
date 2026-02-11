b8ca0525f047c1cfbd62182113cfb1a038d6b4fb
SyedZubairVV <97136875+SyedZubairVV@users.noreply.github.com>
2026-02-10 22:14:20 -0700

    added stored prod to CourseData.cs

--- SchoolDatabaseSystem/Data/CourseData.cs
+++ SchoolDatabaseSystem/Data/CourseData.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace SchoolDatabaseSystem.Data
@@ -8,43 +8,24 @@ namespace SchoolDatabaseSystem.Data
         // Get available sections for a term/year
         public static DataTable GetAvailableSections(string term, int year)
         {
-            string query = @"
-                SELECT
-                    cs.section_id,
-                    c.course_code,
-                    c.title,
-                    cs.section_number,
-                    i.name AS instructor,
-                    cs.max_capacity -
-                        (SELECT COUNT(*) FROM Enrollment e WHERE e.section_id = cs.section_id)
-                        AS seats_remaining
-                FROM CourseSection cs
-                JOIN Course c ON cs.course_id = c.course_id
-                JOIN Instructor i ON cs.instructor_id = i.instructor_id
-                WHERE cs.term = @term
-                  AND cs.year = @year";
-
-            SqlParameter[] parameters = {
-                new SqlParameter("@term", term),
-                new SqlParameter("@year", year)
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Term", term),
+                new SqlParameter("@Year", year)
             };
 
-            return Database.ExecuteQuery(query, parameters);
+            return Database.ExecuteSelectProcedure("sp_GetAvailableSections", parameters);
         }
 
-        // Get course schedule
+        // Get course schedule for a section
         public static DataTable GetCourseSchedule(int sectionId)
         {
-            string query = @"
-                SELECT day_of_week, start_time, end_time
-                FROM CourseSchedule
-                WHERE section_id = @sectionId";
-
-            SqlParameter[] parameters = {
-                new SqlParameter("@sectionId", sectionId)
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@SectionId", sectionId)
             };
 
-            return Database.ExecuteQuery(query, parameters);
+            return Database.ExecuteSelectProcedure("sp_GetCourseSchedule", parameters);
         }
     }
 }
